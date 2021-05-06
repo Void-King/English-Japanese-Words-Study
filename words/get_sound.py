d0e9715d6a2f7ec1861850ab93fcaf10a7ef449c
bzk <bzk@caxa.com>
2021-05-06 13:55:06 +0800

    tweak

--- words/get_sound.py
+++ words/get_sound.py
@@ -6,34 +6,42 @@ import re
 
 import get_trans
 
-def get_s(language_type, single_word, single_cn_word):
+def get_s(language_type, single_word, single_cn_word, single_cn_detail):
     sound_url = ""
     sound_cn_url = ''
-    record_txt = ''
+    record_json = ''
     word_adress = ''
     save_flag = 1
+    cur_json = json.loads("{}")
+    cur_words = {}
     if language_type == 0:# english
         sound_url = 'https://fanyi.baidu.com/gettts?lan=en&text=' + single_word + '&spd=3&source=web'
         sound_cn_url = 'https://fanyi.baidu.com/gettts?lan=zh&text=' + single_cn_word + '&spd=4&source=web'
-        record_txt = './words/word_sound/english.txt'
+        record_json = './words/word_sound/english.json'
         word_adress = './words/word_sound/english/'
-        f = open('./words/word_sound/english.txt','r')
-        for ft in f.readlines():
-            ftn = re.search('  ', ft)
-            if single_word == ft[:ftn.span()[0]]:
+        f = open('./words/word_sound/english.json','r', encoding='utf-8')
+        cur_json = json.loads(f.read())
+        f.close()
+        cur_words = cur_json["words"]
+        for exist in cur_words:
+            if single_word == exist["word"]:
                 # print ('exist!')
                 save_flag = 0
+                break
     elif language_type == 1:# japanese
         sound_url = 'https://fanyi.baidu.com/gettts?lan=jp&text=' + single_word + '&spd=3&source=web'
         sound_cn_url = 'https://fanyi.baidu.com/gettts?lan=zh&text=' + single_cn_word + '&spd=4&source=web'
-        record_txt = './words/word_sound/japanese.txt'
+        record_json = './words/word_sound/japanese.json'
         word_adress = './words/word_sound/japanese/'
-        f = open('./words/word_sound/japanese.txt','r')
-        for ft in f.readlines():
-            ftn = re.search('  ', ft)
-            if single_word == ft[:ftn.span()[0]]:
+        f = open('./words/word_sound/japanese.json','r', encoding='utf-8')
+        cur_json = json.loads(f.read())
+        f.close()
+        cur_words = cur_json["words"]
+        for exist in cur_words:
+            if single_word == exist["word"]:
                 # print ('exist!')
                 save_flag = 0
+                break
     
     if save_flag == 1:
         res = requests.get(sound_url)
@@ -45,8 +53,15 @@ def get_s(language_type, single_word, single_cn_word):
         sound_file.write(res.content)
         sound_file.close()
 
-        record_txt_stream = open(record_txt, 'a')
-        record_txt_stream.write(single_word + '  ' + single_cn_word + '\n')
+        newWordDict = {}
+        newWordDict["word"] = single_word
+        newWordDict["cnword"] = single_cn_word
+        newWordDict["cndetail"] = single_cn_detail
+        cur_words.append(newWordDict)
+
+        outStr = json.dumps(cur_json, ensure_ascii=False)
+        record_txt_stream = open(record_json, 'w', encoding='utf-8')
+        record_txt_stream.write(outStr)
         record_txt_stream.close()
     else:
         pass
--- words/main.py
+++ words/main.py
@@ -11,16 +11,16 @@ import os
 import get_sound
 import get_trans
 
-# AudioSegment.converter = r"D:\\Code\\Python\word\\libav\\usr\bin\\avconv.exe"
-# AudioSegment.ffmpeg = r"D:\\Code\\Python\word\\libav\\usr\bin\\avplay.exe"
-# AudioSegment.ffprobe =r"D:\\Code\\Python\word\\libav\\usr\bin\\avprobe.exe"
-
 work_path = os.getcwd()
 work_path = work_path[::-1]
 work_path = work_path[6:]
 work_path = work_path[::-1]
 print (work_path)
 os.chdir(work_path)
+
+AudioSegment.converter = "./libav/usr/bin/avconv.exe"
+AudioSegment.ffmpeg = "./libav/usr/bin/avplay.exe"
+AudioSegment.ffprobe ="./libav/usr/bin/avprobe.exe"
 
 global questions_num
 questions_num = 0
@@ -36,25 +36,33 @@ def voice_word(word_type, origin_word):
     # print (audio_add + origin_word + '.mp3')
     play(sound)
 
-def mem_test(word_type):
+def word_test(word_type):
     global questions_num
-    questions = []
     questions_voice = []
+    questions = []
+    questions_detail = []
     wordf = ''
     if word_type == 0:
-        wordf = 'english.txt'
+        wordf = 'english.json'
     else:
-        wordf = 'japanese.txt'
-    f = open('./words/word_sound/' + wordf, 'r')
-    for fp in f.readlines():
-        questions.append(fp[re.search('  ', fp).span()[1]:-1])
-        questions_voice.append(fp[:re.search('  ', fp).span()[0]])
+        wordf = 'japanese.json'
+    f = open('./words/word_sound/' + wordf, 'r', encoding='utf-8')
+    cur_json = json.loads(f.read())
+    f.close()
+    cur_words = cur_json["words"]
+    for word in cur_words:
+        questions_voice.append(word["word"])
+        questions.append(word["cnword"])
+        questions_detail.append(word["cndetail"])
     maxlength = len(questions)
     length = maxlength
     if questions_num == 0:
         while (questions_num == 0):
-            inputnum = input ('输入题目基数数量，不超过' + str(maxlength * 2 // 3) + '个:')
+            inputnum = input ('输入题目基数数量，不超过' + str(maxlength * 2 // 3) + '个:(q)')
             try:
+                questions_char = inputnum
+                if questions_char == "q":
+                    return
                 questions_num = int(inputnum)
                 if questions_num % 2 != 0:
                     questions_num -= 1
@@ -71,11 +79,13 @@ def mem_test(word_type):
     b -= 1
     quescon = questions[x]
     quescon_com = questions_voice[x]
+    quescon_detail = questions_detail[x]
     questions.pop(x)
     questions_voice.pop(x)
+    questions_detail.pop(x)
     
     root = tk.Tk()
-    root.geometry('820x310+300+300')
+    root.geometry('818x310+300+300')
     root.resizable(0, 0)
     root.title('Words Study')
     frame1 = tk.Frame(root, background='#3f3f3f')
@@ -87,11 +97,11 @@ def mem_test(word_type):
     answer_com = []
     answer_com.append(quescon_com)
     
-    label1 = tk.Text(frame, width = 22, height = 6, font = '微软雅黑 20')
+    label1 = tk.Text(frame, width = 39, height = 10, font = '微软雅黑 12')
     label1.grid(row = 0, column = 0, sticky = tk.NW, padx = 22, pady = 40)
-    label2 = tk.Text(frame, width = 22, height = 6, font = '微软雅黑 20')
+    label2 = tk.Text(frame, width = 39, height = 10, font = '微软雅黑 12')
     label2.grid(row = 0, column = 1, sticky = tk.NW, padx = 22, pady = 40)
-    label1.insert(INSERT, quescon)
+    label1.insert(INSERT, quescon + "\n\n" + quescon_detail)
 
     def next_ques_re(event):
         next_ques()
@@ -111,8 +121,10 @@ def mem_test(word_type):
         if length >= 0:
             quescon = questions[x]
             quescon_com = questions_voice[x]
+            quescon_detail = questions_detail[x]
             questions.pop(x)
             questions_voice.pop(x)
+            questions_detail.pop(x)
         
         if label2.get('1.0','end')[:-2] == '':
             answer.append('NULL')
@@ -152,7 +164,7 @@ def mem_test(word_type):
             root1.mainloop()
         else:
             answer_com.append(quescon_com)
-            label1.insert(INSERT, quescon)
+            label1.insert(INSERT, quescon + "\n\n" + quescon_detail)
             voice_word(word_type, quescon_com + '_cn')
             voice_word(word_type, quescon_com)
         
@@ -202,35 +214,31 @@ def display_word():
             except:
                 pass
             label2.insert(INSERT, cn_word_all)
-            save_flag = get_sound.get_s(word_type, origin_word, cn_word)
+            save_flag = get_sound.get_s(word_type, origin_word, cn_word, cn_word_all)
             if save_flag == 1:
                 questions_num += 1
                 # print (questions_num)
             voice_word(word_type, origin_word)
-            owEt.delete('0','end')
+            # owEt.delete('0','end')
 
     owBt = tk.Button(frame, text = '查询',command = confirm_word, width = 66, font = "微软雅黑 10")
     owBt.grid(row = 0, column = 1, padx = 15, pady = 25)
 
 
     def engt():
-        mem_test(0)
+        word_test(0)
     def jpt():
         # global questions_num
         # questions_num_com = questions_num * 2 // 3
-        mem_test(1)
+        word_test(1)
     egBt = tk.Button(frame, text = '英语训练',command = engt, width = 50, font = "微软雅黑 10")
     egBt.grid(row = 2, column = 0, padx = 15, pady = 10)
     jpBt = tk.Button(frame, text = '日语训练',command = jpt, width = 50, font = "微软雅黑 10")
     jpBt.grid(row = 2, column = 1, padx = 15, pady = 10)
     
-    # cn_word = get_trans.get_t(0, 'state')
-    # cn_word_all = get_trans.get_allt(0, 'state')
-    # print (cn_word_all)
-    # get_sound.get_s(0, 'state', cn_word)
     root.bind('<Return>', confirm_word_re)
     root.mainloop()
 
     
-# mem_test(0)
+# word_test(0)
 display_word()
